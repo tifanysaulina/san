e34584760cdeec79052945946d8e4cc0e3ea009d
sanfira <60214461+sanfira@users.noreply.github.com>
2020-04-09 19:02:59 +0700

    Membuat FileExternal.C
    Berisikan logika perkiraan fungsi

---
+++ FileExternal.c
@@ -0,0 +1,22 @@
+
+char ReadFile(char *GOL){
+    //open
+    //fgets buat ambil jumlah baris -->ubah ke int
+    //fgets ambil jumlah kolom --> ubah ke int
+    //bikin array[n][m] 
+
+   /* define ROW 5
+    #define COLUMN 10
+    main()
+    {
+    unsigned char **p = NULL, colum = 0;
+    p = malloc ( ROW * sizeof ( unsigned char *) );
+    for (;colum< ROW; ++colum )
+    {
+        p[colum] = malloc (COLUMN * sizeof (unsigned char  ));
+    }
+    }*/
+
+    //outputnya array gol!
+
+}
